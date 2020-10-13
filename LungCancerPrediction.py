ac3788466790205543ad449579a7638874050907
Nazhim <nazhimkalam@gmail.com>
2020-10-13 19:16:37 +0530

    setting up everything and reading the data from the dataset

---
+++ LungCancerPrediction.py
@@ -0,0 +1,13 @@
+
+# Lung Cancer Prediction using Machine Leanring
+
+# Importing the Libraries
+import numpy as np                # creating arrays for linear algebra
+import pandas as pd               # used to create dataframes for data processing using the csv file
+import seaborn as sns             # used for visualization
+import matplotlib.pyplot as plt   # used for plotting data
+
+# Reading the CSV data file
+dataset = pd.read_csv("LungDataSet.csv")
+
+
