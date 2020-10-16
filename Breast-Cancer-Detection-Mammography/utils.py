1e40bb398d8d8701e687dd124875c077ad863854
Ammar <--add>
2020-10-16 16:48:43 +0530

    moved common variables to this utils file

---
+++ Breast-Cancer-Detection-Mammography/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+IMAGE_SIZE = 224
+TEST_BATCH = 16
+TRAIN_BATCH = 9
+VALID_BATCH = 8
+EPOCHS = 30
+
+TRAIN_DIR = 'proper-dataset/train/'
+VALIDATION_DIR = 'proper-dataset/valid/'
+TEST_DIR = 'proper-dataset/test/'
+
