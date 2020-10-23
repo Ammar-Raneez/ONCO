5e7cd56fb25cd3ac7ebf6a90bc3ae68184d23645
Nazhim <nazhimkalam@gmail.com>
2020-10-23 20:05:59 +0530

    setting up the VGG16 model used as a transfer learning method

--- Lung Diseases Detection/Lung Pneumonia Detection/Lung Pneumonia Detection.py
+++ Lung Diseases Detection/Lung Pneumonia Detection/Lung Pneumonia Detection.py
@@ -19,3 +19,58 @@ from keras.models import Sequential
 import numpy as np
 from glob import glob
 import matplotlib.pyplot as plt
+
+
+
+
+# ## Re-sizing all the images
+
+# In[15]:
+
+
+IMAGE_SIZE = [224, 224]
+
+train_path = '../../../../chest_xray/train'
+test_path = '../../../../chest_xray/test'
+
+
+# ## Using VGG16 to create the model
+
+# In[16]:
+
+
+vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top = False)
+
+
+# ### Don't train existing weights
+
+# In[17]:
+
+
+for layer in vgg.layers:
+    layer.trainable = False
+
+
+# ### Getting the number of folders which are my output classes
+
+# In[18]:
+
+
+folders = glob(train_path + '/*')
+folders
+
+
+# ### Creating a flatten layer to flatten the vgg output what ever we got 
+
+# In[19]:
+
+
+x = Flatten()(vgg.output)
+
+
+# ### Adding our last layer
+
+# In[20]:
+
+
+prediction = Dense(len(folders), activation='softmax')(x)
