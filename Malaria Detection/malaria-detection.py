e26a8134f1ec97d59895910edc6a816a20221c7e
Nazhim <nazhimkalam@gmail.com>
2020-10-27 00:22:27 +0530

    using imageGenerator to import the image and work with them

--- Malaria Detection/malaria-detection.py
+++ Malaria Detection/malaria-detection.py
@@ -68,6 +68,26 @@ model.compile(
 )
 
 
+# Use the Image Data Generator to import the images from the dataset'
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                  shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True
+                                  )
+
+# scaling down all my images by turning all the pixel values from 0 to 1
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('cell_images/Train/',
+                                                target_size = (224, 224),
+                                                batch_size=32,
+                                                class_mode='categorical')
+# class_mode is set to categorical because it has 2 classes 'Parasitized' and 'Uninfected'
+
+test_set = test_datagen.flow_from_directory('cell_images/Test/',
+                                                target_size = (224, 224),
+                                                batch_size=32,
+                                                class_mode='categorical')
 
 
 
