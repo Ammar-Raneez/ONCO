9334c467c7659a92a4ef9f29b5e4aaf19f30c5fd
Nazhim <nazhimkalam@gmail.com>
2020-10-26 23:54:50 +0530

    adding the preprocessing layer to front

--- Malaria Detection/malaria-detection.py
+++ Malaria Detection/malaria-detection.py
@@ -25,3 +25,14 @@ import matplotlib.pyplot as plt
 # re-size all the images
 IMAGE_SIZE = [224, 224]
 
+# setting the train and test path files
+train_path = 'cell_images/Train'
+test_path = 'cell_images/Test'
+
+# Add preprocessing layer to front
+vgg = VGG19(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+# NOT training the existing the weights because the weights are already trained and this is the use of transfer learning
+for layer in vgg.layers:
+    layer.trainable = False
+
