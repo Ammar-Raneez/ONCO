430044d01413d911b146882aefc601505ec3bd66
Ammar <ammarraneez@gmail.com>
2020-10-29 15:30:25 +0530

    starting off with multi-class skin cancer detection
    8 different types of skin cancer

---
+++ Skin-Cancer-MultiDiseaseClassification/utils.py
@@ -0,0 +1,24 @@
+TRAIN_DATASET_AK = 'data/Train/actinic keratosis'
+TRAIN_DATASET_BCC = 'data/Train/basal cell carcinoma'
+TRAIN_DATASET_MEL = 'data/Train/melanoma'
+TRAIN_DATASET_NEV = 'data/Train/nevus'
+TRAIN_DATASET_PBK = 'data/Train/pigmented benign keratosis'
+TRAIN_DATASET_SK = 'data/Train/seborrheic keratosis'
+TRAIN_DATASET_SCC = 'data/Train/squamous cell carcinoma'
+TRAIN_DATASET_VL = 'data/Train/vascular lesion'
+
+TEST_DATASET_AK = 'data/Test/actinic keratosis'
+TEST_DATASET_BCC = 'data/Test/basal cell carcinoma'
+TEST_DATASET_MEL = 'data/Test/melanoma'
+TEST_DATASET_NEV = 'data/Test/nevus'
+TEST_DATASET_PBK = 'data/Test/pigmented benign keratosis'
+TEST_DATASET_SK = 'data/Test/seborrheic keratosis'
+TEST_DATASET_SCC = 'data/Test/squamous cell carcinoma'
+TEST_DATASET_VL = 'data/Test/vascular lesion'
+
+TRAIN_DATASET = 'data/Train'
+TEST_DATASET = 'data/Test'
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 250
+
