3a6e6878c25738b6c8fde9c09df8885e2e8c91d3
Ammar <ammarraneez@gmail.com>
2020-10-29 15:24:43 +0530

    Moved common code to utils module

---
+++ Skin-Cancer-Detection-MalignantvsBenign/utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+TEST_MALIGNANT_FOLDER = 'data/test/malignant'
+TEST_BENIGN_FOLDER = 'data/test/benign'
+
+TRAIN_MALIGNANT_FOLDER = 'data/train/malignant'
+TRAIN_BENIGN_FOLDER = 'data/train/benign'
+IMAGE_SIZE = 224
+
