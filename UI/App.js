995a20ac820eb1e54f22b8cf18825cd83c681306
Nazhim <nazhimkalam@gmail.com>
2021-01-18 19:29:47 +0530

    Navigator for the screens(for login and home screen)

--- UI/App.js
+++ UI/App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import { useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MyStack = () => {
+	const [user, setUser] = useState(null);
+
+	return (
+		// This is the Navigator for the screens
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				{!user ? (
+					<>
+						{/* if no user logged in then we display the login component */}
+						<Stack.Screen name="Login" component={Login} />
+					</>
+				) : (
+					<>
+						{/* if the user is logged in then we display the home page component */}
+						<Stack.Screen name="Home" component={Home} />
+					</>
+				)}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default MyStack;
