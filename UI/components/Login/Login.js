bf5706f8efa6d2892a933e1ae7bab8f1ee243eed
Nazhim <nazhimkalam@gmail.com>
2021-01-18 19:43:41 +0530

    added the login section code

---
+++ UI/components/Login/Login.js
@@ -0,0 +1,166 @@
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LockIcon from '@material-ui/icons/Lock';
+import PersonIcon from '@material-ui/icons/Person';
+import React, { useEffect, useState } from 'react';
+import { Alert, Animated, Button, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import LoginWelcome from './LoginWelcome';
+
+const Login = () => {
+	
+
+	// this is to update the displayWelcome component after a given time
+	useEffect(() => {
+		setTimeout(() => {
+			setDisplayWelcome(false);
+			// fading animations
+			Animated.timing(opacity, {
+				toValue: 1,
+				duration: 1500,
+			}).start();
+		}, 5000);
+	}, []);
+
+	const onClickLogin = () => {
+
+	};
+
+	const onClickGoogleLogin = () => {
+
+
+	};
+	const onClickSignUp = () => {
+	
+	};
+
+	const onClickConfirmChangePass = () => {
+		
+		}
+	};
+
+	const onClickForgotPassword = () => {
+		
+	};
+
+	return (
+        <SafeAreaView style={style.container}>
+            {displayWelcome ? (
+				<>
+					<LoginWelcome />
+				</>
+			) : (
+				<>
+					<Animated.View
+						style={{
+							backgroundColor: '#DEF9FF',
+							flex: '1',
+							justifyContent: 'space-between',
+							opacity,
+						}}
+					>
+						{/* header */}
+						<View style={style.login__header}>
+							<Image style={style.logo} source={require('../../assets/oncoAssets/oncoLogo.svg')} />
+						</View>
+
+						{/* welcome back */}
+						<View style={style.login__welcomeBack}>
+							{/* <Image style={style.whiteCloud} source={require('../../assets/oncoAssets/whiteCloud.svg')} /> */}
+							<Text style={style.login__welcomeBackMessage}>Welcome back!</Text>
+						</View>
+
+						{/* login inputs */}
+						<View style={style.login__inputContainer}>
+							{clickedSignUp ? (
+								// THIS SECTION DISPLAYS THE SIGN UP SECTION
+								<>
+									<Text style={style.login__inputContainerLoginDetails}>Create your new account</Text>
+
+									{/* This is the section when the user has clicked the forget password section */}
+
+									{/* USERNAME */}
+									<View
+										style={[
+											style.login__inputContainerInputsSection,
+											!validUserName && style.redField,
+											validUserName && style.blueField,
+										]}
+									>
+										<AccountCircleIcon />
+										<TextInput
+											style={[style.login__inputContainerInputs, !validUserName && style.invalidTextContent]}
+											onChangeText={(text) => {
+												setUsername(text);
+												setValidUserName(true);
+											}}
+											textContentType="name"
+											placeholder="Username"
+											value={username}
+											onFocus={() => {
+												if (!validUserName) {
+													setUsername('');
+												}
+											}}
+										/>
+									</View>
+
+									{/* EMAIL */}
+									<View
+										style={[
+											style.login__inputContainerInputsSection,
+											!validEmail && style.redField,
+											validEmail && style.blueField,
+										]}
+									>
+										<PersonIcon />
+										<TextInput
+											style={[style.login__inputContainerInputs, !validEmail && style.invalidTextContent]}
+											onChangeText={(text) => {
+												setEmail(text);
+												setValidEmail(true);
+											}}
+											textContentType="emailAddress"
+											placeholder="Email Address"
+											value={email}
+											onFocus={() => {
+												if (!validEmail) {
+													setEmail('');
+												}
+											}}
+										/>
+									</View>
+
+									{/* PASSWORD */}
+									<View
+										style={[
+											style.login__inputContainerInputsSection,
+											!validPassword && style.redField,
+											validPassword && style.blueField,
+										]}
+									>
+										<LockIcon />
+										<TextInput
+											style={[style.login__inputContainerInputs, !validPassword && style.invalidTextContent]}
+											onChangeText={(text) => {
+												setPassword(text);
+												setValidPassword(true);
+											}}
+											textContentType="password"
+											placeholder="Password"
+											secureTextEntry={validPassword && true}
+											value={password}
+											onFocus={() => {
+												if (!validPassword) {
+													setPassword('');
+												}
+											}}
+										/>
+									</View>
+								</>
+        </SafeAreaView>
+	);
+};
+const style = StyleSheet.create({
+	
+});
+export default Login;
