068a97dec26cbb888f7c6da5ee18c344b8d6abc7
Nazhim <nazhimkalam@gmail.com>
2021-01-24 18:27:17 +0530

    added all the related routes for the login functionality

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ui/screens/changePassword_screeen.dart';
+import 'package:ui/screens/home_screen.dart';
+import 'package:ui/screens/login_screen.dart';
+import 'package:ui/screens/registration_screen.dart';
+import 'package:ui/screens/welcome_screen.dart';
 
 // Main method CALLED at which the APP STARTS INITIALLY
 void main()  => runApp( MyApp() );
@@ -7,6 +12,22 @@ void main()  => runApp( MyApp() );
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+
+      // This is the first screen which will be displayed for the user when he opens the app
+      home: WelcomeScreen(),
+
+      // This is the initial route for the app
+      initialRoute: WelcomeScreen.id,
+
+      // Creating Named routes for all the pages (we used named routes when we deal with multiple routes 'more than 2')
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChangePassword.id: (context) => ChangePassword(),
+        HomeScreen.id: (context) => HomeScreen()
+      },
+    )
   }
 }
