0739e06f499c86eef612ed9773cd7460a351ae3e
maneesha14w <maneesha14w@gmail.com>
2021-02-02 20:39:54 +0530

    optimised imports + added proper comments for cancer_page

--- ui/lib/screens/cancer_page.dart
+++ ui/lib/screens/cancer_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:ui/components/cancer_card.dart'; //imports card component
 
-//TODO add top appbar/row and add
+//TODO add top appbar/row and add functionality for passing colors into card.
 
+/* This is the Cancer Page that displays an appbar with a gradient and a logo, three cards that allow the user to click on a
+ certain cancer and be redirected to that page and the nav at the bottom. */
 class CancerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,6 +12,7 @@ class CancerPage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.only(
+            //* might need to change when adding top row and nav
             top: 120.0,
             left: 24.0,
             right: 24.0,
@@ -23,11 +27,18 @@ class CancerPage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 32,),
+                SizedBox(
+                  height: 32,
+                ),
                 CancerCard(cardTitle: 'Skin Cancer.'),
-                SizedBox(height: 32,),
+                //generates a cancer card with text passed.
+                SizedBox(
+                  height: 32,
+                ),
                 CancerCard(cardTitle: 'Lung Cancer.'),
-                SizedBox(height: 32,),
+                SizedBox(
+                  height: 32,
+                ),
                 CancerCard(cardTitle: 'Breast Cancer.'),
               ],
             ),
@@ -37,4 +48,3 @@ class CancerPage extends StatelessWidget {
     );
   }
 }
-
