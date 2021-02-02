cd83625740a3bb5bcec78f492a93adfe4e88451e
maneesha14w <maneesha14w@gmail.com>
2021-02-02 20:23:36 +0530

    added cancer card component.
    + this component can be reused for making cards, you can pass the text
    + TODO increase functionality to pass colors as well

---
+++ ui/lib/components/cancer_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CancerCard extends StatelessWidget {
+  final String cardTitle;
+
+  CancerCard({@required this.cardTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Container(
+        width: 320.0,
+        height: 120.0,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 80.0, left: 20.0),
+          child: Text(
+            cardTitle,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color(0xFF345860),
+            ),
+          ),
+        ),
+      ),
+      color: Color(0xFFd3e5e8),
+    );
+    // width: 320.0,
+    // height: 120.0,
+    // decoration: BoxDecoration(
+    //   borderRadius: BorderRadius.circular(30.0),
+    //   color: Color(0xFFd3e5e8),
+    // ),
+  }
+}
