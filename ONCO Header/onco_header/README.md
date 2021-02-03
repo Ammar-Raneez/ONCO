4e238f036cd9a441af60da31138efff910bc2833
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-02-03 14:19:02 +0530

    Deleted App Bar

--- ONCO Header/onco_header/README.md
+++
@@ -1,3 +0,0 @@
-# Header Part of Flutter App 
-
-Contains Header Part of Flutter App with only the Settings Icon
--- ONCO Header/onco_header/android/app/src/main/java/oncoheader/onco_header/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package oncoheader.onco_header;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
