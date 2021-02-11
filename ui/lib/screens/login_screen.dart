c9d55f61b83337f6f1fda937ff2b04ff5825ea7a
Nazhim <nazhimkalam@gmail.com>
2021-02-11 10:46:56 +0530

    using WillPopScope to prevent default control panel disrupting the flutter app

--- ui/lib/screens/login_screen.dart
+++ ui/lib/screens/login_screen.dart
@@ -120,176 +120,179 @@ class _LoginScreenState extends State<LoginScreen> {
     double keyboardOpenVisibility = MediaQuery.of(context).viewInsets.bottom;
 
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: ModalProgressHUD(
-          inAsyncCall: showSpinner,
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: kBackgroundBlueGradient,
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 20.0,
-                horizontal: 20.0,
+      child: WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: ModalProgressHUD(
+            inAsyncCall: showSpinner,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: kBackgroundBlueGradient,
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Flexible(
-                    child: Hero(
-                      tag: "logo",
-                      child: Container(
-                        height: 20,
-                        child: Image.asset('images/officialLogo.png'),
-                      ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 20.0,
+                  horizontal: 20.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      height: 8.0,
                     ),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  if (keyboardOpenVisibility == 0.0)
                     Flexible(
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          Container(
-                            height: 150,
-                            child: Image.asset('images/clouds.png'),
-                          ),
-                          Text(
-                            "Welcome back!",
-                            textAlign: TextAlign.center,
-                            style: kTextStyle.copyWith(
-                              color: Color(0xff5b5b5b),
-                              fontSize: 20,
+                      child: Hero(
+                        tag: "logo",
+                        child: Container(
+                          height: 20,
+                          child: Image.asset('images/officialLogo.png'),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    if (keyboardOpenVisibility == 0.0)
+                      Flexible(
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              height: 150,
+                              child: Image.asset('images/clouds.png'),
                             ),
-                          ),
-                        ],
+                            Text(
+                              "Welcome back!",
+                              textAlign: TextAlign.center,
+                              style: kTextStyle.copyWith(
+                                color: Color(0xff5b5b5b),
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  if (keyboardOpenVisibility == 0.0)
-                    Text(
-                      "Log in to your existing account",
-                      textAlign: TextAlign.center,
-                      style: kTextStyle.copyWith(
-                        fontSize: 15,
+                    if (keyboardOpenVisibility == 0.0)
+                      Text(
+                        "Log in to your existing account",
+                        textAlign: TextAlign.center,
+                        style: kTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
                       ),
-                    ),
-                  TextField(
-                    controller: _emailAddressTextFieldController,
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (value) {
-                      email = value;
-                    },
-                    decoration: kTextFieldDecoration.copyWith(
-                      hintText: "Enter your email",
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.lightBlueAccent,
-                      ),
-                    ),
-                  ),
-                  TextField(
-                    controller: _passwordTextFieldController,
-                    obscureText: !visiblePassword,
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    decoration: kTextFieldDecoration.copyWith(
-                      hintText: "Enter your password",
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.lightBlueAccent,
-                      ),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            visiblePassword = !visiblePassword;
-                          });
-                        },
-                        child: Icon(
-                          Icons.remove_red_eye,
+                    TextField(
+                      controller: _emailAddressTextFieldController,
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value) {
+                        email = value;
+                      },
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter your email",
+                        prefixIcon: Icon(
+                          Icons.person,
                           color: Colors.lightBlueAccent,
                         ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // GO TO THE FORGET PASSWORD SCREEN
-                      Navigator.pushNamed(context, ForgetPassword.id);
-                    },
-                    child: Text(
-                      "Forgot Password?",
-                      textAlign: TextAlign.end,
+                    TextField(
+                      controller: _passwordTextFieldController,
+                      obscureText: !visiblePassword,
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter your password",
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.lightBlueAccent,
+                        ),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              visiblePassword = !visiblePassword;
+                            });
+                          },
+                          child: Icon(
+                            Icons.remove_red_eye,
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // GO TO THE FORGET PASSWORD SCREEN
+                        Navigator.pushNamed(context, ForgetPassword.id);
+                      },
+                      child: Text(
+                        "Forgot Password?",
+                        textAlign: TextAlign.end,
+                        style: kTextStyle.copyWith(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    RoundedButton(
+                      onPressed: () {
+                        //Implement login functionality.
+                        loginUserByEmailAndPassword(context);
+                        // clearing the text fields on submitted the details
+                        _emailAddressTextFieldController.clear();
+                        _passwordTextFieldController.clear();
+                      },
+                      colour: Colors.lightBlueAccent,
+                      title: 'LOG IN',
+                    ),
+                    Text(
+                      "Or connect using",
+                      textAlign: TextAlign.center,
                       style: kTextStyle.copyWith(
                         fontSize: 12,
                       ),
                     ),
-                  ),
-                  RoundedButton(
-                    onPressed: () {
-                      //Implement login functionality.
-                      loginUserByEmailAndPassword(context);
-                      // clearing the text fields on submitted the details
-                      _emailAddressTextFieldController.clear();
-                      _passwordTextFieldController.clear();
-                    },
-                    colour: Colors.lightBlueAccent,
-                    title: 'LOG IN',
-                  ),
-                  Text(
-                    "Or connect using",
-                    textAlign: TextAlign.center,
-                    style: kTextStyle.copyWith(
-                      fontSize: 12,
+                    RoundedButton(
+                      onPressed: () {
+                        //Implement login functionality using google auth
+                        googleAuthLogin(context);
+                      },
+                      colour: Colors.redAccent,
+                      title: 'Google',
                     ),
-                  ),
-                  RoundedButton(
-                    onPressed: () {
-                      //Implement login functionality using google auth
-                      googleAuthLogin(context);
-                    },
-                    colour: Colors.redAccent,
-                    title: 'Google',
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have an account?",
-                          style: kTextStyle.copyWith(
-                            fontSize: 13,
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style: kTextStyle.copyWith(
+                              fontSize: 13,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            print("U tapped the sign up button");
-                            // GO TO THE SIGN IN SCREEN
-                            Navigator.pushNamed(context, RegistrationScreen.id);
-                          },
-                          child: Text("Sign Up",
-                              style: kTextStyle.copyWith(
-                                color: Color(0xff01CDFA),
-                                fontSize: 13,
-                              )),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              print("U tapped the sign up button");
+                              // GO TO THE SIGN IN SCREEN
+                              Navigator.pushNamed(context, RegistrationScreen.id);
+                            },
+                            child: Text("Sign Up",
+                                style: kTextStyle.copyWith(
+                                  color: Color(0xff01CDFA),
+                                  fontSize: 13,
+                                )),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
