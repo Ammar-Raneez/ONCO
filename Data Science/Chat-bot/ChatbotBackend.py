6662a981b98db2c81e6af862be67f9c7fdb8751a
Ammar <ammarraneez@gmail.com>
2021-02-11 13:37:01 +0530

    Create ChatbotBackend.py
    created Flask backend for API communication

---
+++ Data Science/Chat-bot/ChatbotBackend.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import numpy as np
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+from ChatbotFunctions import ChatbotFunctions
+
+app = Flask(__name__)
+chatbot_functions = ChatbotFunctions()
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+api = Api(app)
+model = chatbot_functions.create_model()
+
+class ChatbotChat(Resource):
+    @staticmethod
+    def post():
+        username = "bro"
+        parser = reqparse.RequestParser()
+        parser.add_argument('UserIn')
+        args = parser.parse_args()
+        print(args['UserIn'])
+        chatbot_response = {'Chatbot Response' : chatbot_functions.chat( args['UserIn'], username, model)}
+        return chatbot_response, 200
+
+api.add_resource(ChatbotChat, '/chatbot-predict')
+
+if __name__ == '__main__':
+    app.run(debug=True)
