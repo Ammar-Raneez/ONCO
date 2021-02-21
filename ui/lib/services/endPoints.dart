c210008bd7c2cd118ac594398201d607858b5cbe
Nazhim <nazhimkalam@gmail.com>
2021-02-21 22:30:56 +0530

    added service dir for API endpoints location

---
+++ ui/lib/services/endPoints.dart
@@ -0,0 +1,8 @@
+// API END POINTS
+const postLungCancerPrediction_API = "";
+const postSkinCancerPrediction_API = "";
+const postBreastCancerPrediction_API = "";
+
+const postLungCancerDetection_API = "";
+const postSkinCancerDetection_API = "";
+const postBreastCancerDetection_API = "";
