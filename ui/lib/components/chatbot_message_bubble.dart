c28e4cffe0feff31cc956ad72de2fc6e9181040a
Ammar <ammarraneez@gmail.com>
2021-03-04 13:59:32 +0530

    created message bubbles for chat bot, and refactored widgets to naming conventions

---
+++ ui/lib/components/chatbot_message_bubble.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String messageSender;
+  final String messageText;
+  final bool isMe;
+
+  MessageBubble({this.messageSender, this.messageText, this.isMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            messageSender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            ),
+          ),
+          Material(
+            borderRadius: isMe ?
+            BorderRadius.only(topLeft: Radius.circular(30.0), bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0))
+                :
+            BorderRadius.only(bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0), topRight: Radius.circular(30.0)),
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                '$messageText',
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- ui/lib/screens/diagnosis/lungDiagnosis_screen.dart
+++ ui/lib/screens/diagnosis/lungDiagnosis_screen.dart
@@ -4,8 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:ui/components/AlertWidget.dart';
-import 'package:ui/components/RoundedButton.dart';
+import 'package:ui/components/alert_widget.dart';
+import 'package:ui/components/rounded_button.dart';
 import 'package:ui/constants.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
