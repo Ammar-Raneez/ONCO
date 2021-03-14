dc126da57709c0d29e44442e3d382f98810db518
Hammadh Arquil <hammadhy11@gmail.com>
2021-03-14 21:01:03 +0530

    Created Add Medication Screen

---
+++ ui/lib/screens/addMedication_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddMedication extends StatefulWidget {
+  @override
+  _AddMedicationState createState() => _AddMedicationState();
+}
+
+class _AddMedicationState extends State<AddMedication> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
