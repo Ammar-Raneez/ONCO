f5020e462aabe1db4025cbc83f18b6fdeaf881b3
Maneesha <maneesha14w@gmail.com>
2021-03-19 12:55:16 +0530

    added back btn with styling.

--- ui/lib/screens/selectService_screen.dart
+++ ui/lib/screens/selectService_screen.dart
@@ -19,30 +19,74 @@ class _SelectServiceScreenState extends State<SelectServiceScreen> {
             ),
             child: Center(
               child: Column(children: [
-                Text("Select an option", style: TextStyle(
-                  fontFamily: 'Poppins-SemiBold',
-                  fontSize: 27.0,
-                  color: Color(0xFF565D5E),
+                Text(
+                  "Select an option",
+                  style: TextStyle(
+                    fontFamily: 'Poppins-SemiBold',
+                    fontSize: 27.0,
+                    color: Color(0xFF565D5E),
+                  ),
                 ),
+                Text(
+                  "Type Of Cancer",
+                  style: TextStyle(
+                    fontFamily: 'Poppins-SemiBold',
+                    fontSize: 27.0,
+                    color: Color(0xFF93ACB1),
+                  ),
                 ),
-                Text("Type Of Cancer", style: TextStyle(
-                  fontFamily: 'Poppins-SemiBold',
-                  fontSize: 27.0,
-                  color: Color(0xFF93ACB1),
+                SizedBox(
+                  height: 35,
                 ),
-                ),
-                SizedBox(height: 15,),
                 TreatmentCard(cardTitle: 'Diagnosis'),
-                SizedBox(height: 2,),
-                Text("OR", style: TextStyle(
-                  fontFamily: 'Poppins-SemiBold',
-                  fontSize: 18.0,
-                  color: Color(0xFF959595),
+                SizedBox(
+                  height: 2,
                 ),
+                Text(
+                  "OR",
+                  style: TextStyle(
+                    fontFamily: 'Poppins-SemiBold',
+                    fontSize: 18.0,
+                    color: Color(0xFF959595),
+                  ),
                 ),
-                SizedBox(height: 2,),
+                SizedBox(
+                  height: 2,
+                ),
                 TreatmentCard(cardTitle: 'Prognosis'),
-                SizedBox(height: 25,)
+                SizedBox(
+                  height: 46,
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    decoration: ShapeDecoration(
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF01D1FF), Color(0xFF006377)],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0),),
+                    ),
+                     padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                     width: 250.0,
+                    height: 65.0,
+                    child: Row(
+                      children: [
+                        new Icon(Icons.arrow_back),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Back To Home Page",
+                          style: TextStyle(
+                              fontSize: 17.0, fontFamily: 'Poppins-Regular', color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ]),
             ),
           ),
@@ -51,4 +95,3 @@ class _SelectServiceScreenState extends State<SelectServiceScreen> {
     );
   }
 }
-
