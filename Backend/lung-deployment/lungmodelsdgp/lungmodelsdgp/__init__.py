700206152341dceb2914a195f70dbdc437bdcf9e
Ammar <ammarraneez@gmail.com>
2021-03-26 11:53:33 +0530

    replicated init and app files from skin cancer deployment for initalization

--- Backend/lung-deployment/lungmodelsdgp/lungmodelsdgp/__init__.py
+++ Backend/lung-deployment/lungmodelsdgp/lungmodelsdgp/__init__.py
@@ -1,24 +1,57 @@
-import logging
-
+import numpy as np
 import azure.functions as func
+import cv2
+import json
+from .app import upload, get_firebase_image
+from azure.storage.blob import BlobServiceClient, BlobClient
+from pyrebase import pyrebase
+import uuid
 
+# Initializing Firebase App
+firebase_config = {
+    "apiKey": "AIzaSyCTCf6EWg_Mthy590c8JpkX5CsOdIlpEA4",
+    "authDomain": "onco-127df.firebaseapp.com",
+    "projectId": "onco-127df",
+    "databaseURL": "https://onco-127df-default-rtdb.firebaseio.com",
+    "storageBucket": "onco-127df.appspot.com",
+    "messagingSenderId": "425803825049",
+    "appId": "1:425803825049:web:3f261cd2ecf11241065d08",
+    "measurementId": "G-S3DZNYJLXT"
+}
+# firebase will store the image to be accessed in the frontend for the report generation
+firebase = pyrebase.initialize_app(firebase_config)
+firebase_storage = firebase.storage()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    headers = {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    #fetching the file passed in through form data
+    fetched_file = req.files['file']
+    # make a unique filename, to avoid replacement upon same name uploads
+    filename = str(uuid.uuid4()) + ".jpg"
+    #converting the file into a byte stream
+    filestream = fetched_file.stream
+    filestream.seek(0)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # save image to azure storage blob
+    #wrap inside try and catch to prevent errors thrown upon same image trying to be saved
+    try:
+        blob = BlobClient.from_connection_string(conn_str= "DefaultEndpointsProtocol=https;AccountName=skinmodelsdgp;AccountKey=WugXQYizUnx2W7Apf/RQV0wVtV29nI2GhG1ZiD3SsryK887JvGj/N0zJZIy0cgOwWRNAy3ggdLCRE0X8vUN2Cg==", container_name="images", blob_name=filename)
+        blob.upload_blob(filestream.read(), blob_type="BlockBlob")
+    except:                                                                                                                                                                          
+        pass
+
+    which_model = req.params.get('model')
+    blob_data = blob.download_blob()
+    blob_data_as_bytes = blob_data.content_as_bytes()
+
+    #convert it into a numpy array, so that it can be passed into opencv
+    np_blob_array = np.fromstring(blob_data_as_bytes, dtype='uint8')
+    prediction = upload(np_blob_array, which_model)
+
+    # getting download image URL
+    image_url = get_firebase_image(filename, filestream.read(), firebase_storage, firebase)
+    return func.HttpResponse(json.dumps([{"imageDownloadURL": image_url, "result_string": prediction}]), status_code = 200, headers = headers)
---
+++ Backend/lung-deployment/lungmodelsdgp/lungmodelsdgp/app.py
@@ -0,0 +1,53 @@
+import os
+import logging
+import tensorflow as tf
+from .skinDiagModule import SkinDiagModule
+
+skinDiagModule = SkinDiagModule()
+
+scriptpath = os.path.abspath(__file__)
+scriptdir = os.path.dirname(scriptpath)
+SKIN_MODEL_PATH = os.path.join(scriptdir, 'skin_model.hdf5')
+
+
+# upload image to firebase storage
+def get_firebase_image(image_name, image_stream, firebase_storage, firebase):
+    # storing the image from local path to the firebase cloud storage
+    firebase_storage.child("skin-image-uploads/" + image_name).put(image_stream)
+    
+    # get the downloadable url and return it
+    auth = firebase.auth()
+    email = "onconashml@gmail.com"
+    password = "onconashml12345"
+    user = auth.sign_in_with_email_and_password(email, password)
+    url = firebase_storage.child(image_name).get_url(user["idToken"])
+    return url
+
+def construct_skin_output(prediction):
+    INDEX_TO_TYPE = {
+        0: 'Melanocytic nevi',
+        1: 'Melanoma',
+        2: 'Benign keratosis-like lesions ',
+        3: 'Basal cell carcinoma',
+        4: 'Actinic keratoses',
+        5: 'Vascular lesions',
+        6: 'Dermatofibroma'
+    }
+        
+    result_string = ""
+    for i in range(len(prediction.flat)):
+        result_string += str(round(prediction.flat[i], 2)) + f"% {INDEX_TO_TYPE[i]} \n"
+    return result_string
+
+def model_predict(image_array, model):
+    if model == "skin":
+        skin_model = tf.keras.models.load_model(SKIN_MODEL_PATH)
+        predictions = skinDiagModule.model_predict_skin(image_array, skin_model)
+        return construct_skin_output(predictions)
+    elif model == "breast":
+        return "breast"
+    return "lung"
+
+def upload(image_array, which_model):        
+    prediction = model_predict(image_array, which_model)
+    return prediction
