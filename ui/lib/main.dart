8ea80cfb85e723a73801030d15cea4dc2ae22179
Hammadh Arquil <hammadhy11@gmail.com>
2021-04-06 21:53:20 +0530

    Updated main.dart to use Flutter Provider

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ui/screens/Personal%20Manager/api/medicationsProvider.dart';
 import 'package:ui/screens/diagnosis/breastDiagnosis_screen.dart';
 import 'package:ui/screens/diagnosis/lungDiagnosis_screen.dart';
 import 'package:ui/screens/diagnosis/skinDiagnosis_screen.dart';
@@ -8,17 +11,22 @@ import 'package:ui/screens/login_screen.dart';
 import 'package:ui/screens/registration_screen.dart';
 import 'package:ui/screens/welcome_screen.dart';
 
-// Main method CALLED at which the APP STARTS INITIALLY
-void main() {
-  // String name = "";
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
-// This is the Main MyApp class for the navigation lists
 class MyApp extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => MedicationProvider(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       // This is the first screen which will be displayed for the user when he opens the app
       home: WelcomeScreen(),
 
@@ -35,7 +43,43 @@ class MyApp extends StatelessWidget {
         LungCancerDiagnosis.id: (context) => LungCancerDiagnosis(),
         BreastCancerDiagnosis.id: (context) => BreastCancerDiagnosis(),
         SkinCancerDiagnosis.id: (context) => SkinCancerDiagnosis(),
-      },
-    );
-  }
+      }
+    ),
+  );
 }
+
+
+
+
+
+// // Main method CALLED at which the APP STARTS INITIALLY
+// void main() {
+//   // String name = "";
+//   runApp(MyApp());
+// }
+//
+// // This is the Main MyApp class for the navigation lists
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       // This is the first screen which will be displayed for the user when he opens the app
+//       home: WelcomeScreen(),
+//
+//       // This is the initial route for the app
+//       initialRoute: WelcomeScreen.id,
+//
+//       // Creating Named routes for all the pages (we used named routes when we deal with multiple routes 'more than 2')
+//       routes: {
+//         WelcomeScreen.id: (context) => WelcomeScreen(),
+//         LoginScreen.id: (context) => LoginScreen(),
+//         RegistrationScreen.id: (context) => RegistrationScreen(),
+//         ForgetPassword.id: (context) => ForgetPassword(),
+//         CurrentScreen.id: (context) => CurrentScreen(),
+//         LungCancerDiagnosis.id: (context) => LungCancerDiagnosis(),
+//         BreastCancerDiagnosis.id: (context) => BreastCancerDiagnosis(),
+//         SkinCancerDiagnosis.id: (context) => SkinCancerDiagnosis(),
+//       },
+//     );
+//   }
+// }
