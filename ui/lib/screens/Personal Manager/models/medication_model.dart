0bfc571b70eeb742dbcfc6f8ea36e370956a68b6
Hammadh Arquil <hammadhy11@gmail.com>
2021-04-06 21:57:27 +0530

    Updated Medication model

--- ui/lib/screens/Personal Manager/models/medication_model.dart
+++ ui/lib/screens/Personal Manager/models/medication_model.dart
@@ -1,14 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:ui/screens/Personal%20Manager/api/utils.dart';
+
+class MedicationTime {
+  static const createdTime = 'createdTime';
+}
+
 class Medication {
-  final int id;
-  final String title;
 
-  Medication({this.id,this.title}); // Constructor
+  String id;
+  String medicationName;
+  String dosage;
+  String doseTime;
 
-  Map<String, dynamic> toMap(){
-    return{
-      'id':id,
-      'title':title
-    };
-  }
+  Medication({
+    @required this.medicationName,
+    this.dosage = '',
+    this.doseTime='',
+    this.id,
+  }); // Constructor
+
+  static Medication fromJson(Map<String, dynamic> json) => Medication(
+    medicationName: json['medicationName'],
+    dosage: json['dosage'],
+    doseTime: json['doseTime'],
+    id: json['id'],
+  );
+
+  Map<String, dynamic> toJson() => {
+    'medicationName': medicationName,
+    'dosage': dosage,
+    'doseTime': doseTime,
+    'id': id,
+  };
 
 }
