af1cacc07162087e03e434c746f51e6ebedb79b2
Hammadh Arquil <hammadhy11@gmail.com>
2021-04-16 15:07:44 +0530

    Created new Directory for Report files

---
+++ ui/lib/screens/Personal Manager/reportManager/reports_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:ui/components/custom_app_bar.dart';
+import 'package:ui/components/reports_card.dart';
+
+import 'viewReport_screen.dart';
+
+class ReportManager extends StatefulWidget {
+  @override
+  _ReportManagerState createState() => _ReportManagerState();
+}
+
+class _ReportManagerState extends State<ReportManager> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: CustomAppBar.arrow(context),
+        body: SafeArea(
+          child: Container(
+            child:Column(
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 20,
+                            bottom: 8,
+                          ),
+                          child: Align(
+
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Reports",
+                              style: TextStyle(
+                                fontFamily: 'Poppins-SemiBold',
+                                fontSize: 32,
+                                color: Color(0xFF00404E)
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20,
+                              right: 20,
+                              bottom:15
+                          ),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Here you can manage your medications, click add a medication to create one and drag the medication to delete it",
+                              style: TextStyle(
+                                  fontFamily: 'Poppins-SemiBold',
+                                  fontSize: 13.0,
+                                  color: Color(0xFF3C707B)
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ViewReport() // Navigates to Task Page
+                        ),
+                      ).then((value){
+                        setState(() {}); // Setting and Refreshing State
+                      });
+                    },
+                    child: ReportCard(reportDate: "28th April 2021", reportType: "Lung Cancer Prognosis")
+                ),
+                ReportCard(reportDate: "24th April 2021", reportType: "Breast Cancer Prognosis"),
+                ReportCard(reportDate: "5th April 2021", reportType: "Lung Cancer Diagnosis"),
+
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ ui/lib/screens/Personal Manager/reportManager/viewReport_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:ui/components/custom_app_bar.dart';
+import 'package:ui/screens/Personal%20Manager/reportManager/models/prognosis_report.dart';
+
+class ViewReport extends StatefulWidget {
+  @override
+  _ViewReportState createState() => _ViewReportState();
+}
+
+class _ViewReportState extends State<ViewReport> {
+  var reportType="Name";
+  var reportDate="Date";
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: CustomAppBar.arrow(context),
+        body: SafeArea(
+          child: Container(
+            child:Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10,
+                        left: 20,
+                      ),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          reportDate,
+                          style: TextStyle(
+                              fontFamily: 'Poppins-SemiBold',
+                              fontSize: 27,
+                              color: Color(0xFF00404E)
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                          bottom:15
+                      ),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          reportType,
+                          style: TextStyle(
+                              fontFamily: 'Poppins-SemiBold',
+                              fontSize: 19,
+                              color: Color(0xFF3C707B)
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    top: 15,
+                    bottom: 10
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("INPUTS PROVIDED",
+                      style: TextStyle(
+                          fontFamily: 'Poppins-SemiBold',
+                          fontSize: 18,
+                          color: Color(0xFF7CC2C4)
+                      ),
+                    ),
+                  ),
+                ),
+
+                ListView(
+                  children: [
+                    Container(
+
+                      child: StreamBuilder<List<Prognosis>>(
+                          // stream: ReportFirebaseApi.),
+                          builder: (context, snapshot) {
+                            switch (snapshot.connectionState) {
+                              case ConnectionState.waiting:
+                                return Center(
+                                    child: CircularProgressIndicator());
+                              default:
+                                if (snapshot.hasError) {
+                                  return buildText(
+                                      'Something went wrong, Try later');
+                                } else {
+                                  final reports = snapshot.data;
+
+                                  final provider = Provider.of<MedicationProvider>(context);
+                                  provider.setMedications(medications);
+
+                                  return MedicationListWidget();
+                                }
+                            }
+                          }
+                      ),
+                  ],
+                )
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
