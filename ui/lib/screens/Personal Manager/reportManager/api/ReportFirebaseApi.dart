df3124b295b2e528872dafdb200fcadb80e1bb92
Hammadh Arquil <hammadhy11@gmail.com>
2021-04-16 16:40:53 +0530

    Updated import statements

--- ui/lib/screens/Personal Manager/reportManager/api/ReportFirebaseApi.dart
+++ ui/lib/screens/Personal Manager/reportManager/api/ReportFirebaseApi.dart
@@ -1,8 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:ui/screens/Personal%20Manager/medicationManager/api/utils.dart';
 import 'package:ui/screens/Personal%20Manager/reportManager/models/report.dart';
+import 'package:ui/screens/Personal%20Manager/utils.dart';
 import 'package:ui/services/GoogleUserSignInDetails.dart';
 
 final user = FirebaseAuth.instance.currentUser;
