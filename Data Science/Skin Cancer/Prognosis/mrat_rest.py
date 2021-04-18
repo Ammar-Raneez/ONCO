d1e16b505a581a751198613645139f72dcd26e72
sabiq <sabiqsabry48@gmail.com>
2021-04-18 15:51:26 +0530

    obtain incidence and mortality rates from mrat constants

--- Data Science/Skin Cancer/Prognosis/mrat_rest.py
+++ Data Science/Skin Cancer/Prognosis/mrat_rest.py
@@ -11,16 +11,23 @@ class MelanomaRiskAssessmentTool:
         elif (parameters['gender'] == 'Female'):
             sex = 1
 
-        r = 1
+        absolute_risk = 1
         if sex == 0:
-            r *= MRATConstants.SUNBURN[int(parameters['sunburn'] )]
-            r *= MRATConstants.MALE_COMPLEXION[int(parameters['complexion'] )]
-            r *= MRATConstants.BIG_MOLES[int(parameters['big-moles'] )]
-            r *= MRATConstants.MALE_SMALL_MOLES[int(parameters['small-moles'] )]
-            r *= MRATConstants.MALE_FRECKLING[int(parameters['freckling'] )]
-            r *= MRATConstants.DAMAGE[int(parameters['damage'] )]
+            absolute_risk *= MRATConstants.SUNBURN[int(parameters['sunburn'] )]
+            absolute_risk *= MRATConstants.MALE_COMPLEXION[int(parameters['complexion'] )]
+            absolute_risk *= MRATConstants.BIG_MOLES[int(parameters['big-moles'] )]
+            absolute_risk *= MRATConstants.MALE_SMALL_MOLES[int(parameters['small-moles'] )]
+            absolute_risk *= MRATConstants.MALE_FRECKLING[int(parameters['freckling'] )]
+            absolute_risk *= MRATConstants.DAMAGE[int(parameters['damage'] )]
         else:
-            r *= MRATConstants.TAN[int(parameters['tan'] )]
-            r *= MRATConstants.FEMALE_COMPLEXION[int(parameters['complexion'] )]
-            r *= MRATConstants.FEMALE_SMALL_MOLES[int(parameters['small-moles'] )]
-            r *= MRATConstants.FEMALE_FRECKLING[int(parameters['freckling'] )]
+            absolute_risk *= MRATConstants.TAN[int(parameters['tan'] )]
+            absolute_risk *= MRATConstants.FEMALE_COMPLEXION[int(parameters['complexion'] )]
+            absolute_risk *= MRATConstants.FEMALE_SMALL_MOLES[int(parameters['small-moles'] )]
+            absolute_risk *= MRATConstants.FEMALE_FRECKLING[int(parameters['freckling'] )]
+
+        # to get incidence values
+        age_index = int((age - 20) / 5)
+        t1 = age_index * 5 + 20
+        t2 = t1 + 5
+        incident_rate = MRATConstants.SEX[sex] * MRATConstants.INCIDENCE[sex][age_index]
+        mortality_rate = MRATConstants.MORTALITY[sex][age_index]
