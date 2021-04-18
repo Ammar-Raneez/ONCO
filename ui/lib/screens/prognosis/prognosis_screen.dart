3d12e641cb7c244e6b67a9328d6a08c942ffaaf7
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-04-18 15:29:27 +0530

    Styled the Progress Dialog Further

--- ui/lib/screens/prognosis/prognosis_screen.dart
+++ ui/lib/screens/prognosis/prognosis_screen.dart
@@ -313,17 +313,26 @@ class _CancerPrognosisState extends State<CancerPrognosis> {
                       // Styling Progress Dialog
                       progressDialog.style(
                           message: '   Analyzing Input',
+                          padding: EdgeInsets.all(20),
                           borderRadius: 10.0,
                           backgroundColor: Colors.white,
-                          progressWidget: CircularProgressIndicator(),
+                          progressWidget: LinearProgressIndicator(backgroundColor: Colors.lightBlueAccent,),
                           elevation: 10.0,
-                          insetAnimCurve: Curves.easeInOut,
+                          insetAnimCurve: Curves.easeInCubic,
                           progress: 0.0,
                           maxProgress: 100.0,
                           progressTextStyle: TextStyle(
-                              color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
+                            color: Colors.black,
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Poppins-SemiBold',
+                          ),
                           messageTextStyle: TextStyle(
-                              color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600)
+                            color: Colors.black,
+                            fontSize: 19.0,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Poppins-SemiBold',
+                          )
                       );
 
                       progressDialog.show();
