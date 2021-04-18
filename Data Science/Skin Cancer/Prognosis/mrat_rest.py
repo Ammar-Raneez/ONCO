77e2746646b2115e5e21164e00c1b451e8e3dba0
sabiq <sabiqsabry48@gmail.com>
2021-04-18 15:54:54 +0530

    Update mrat_rest.py
    change risk variable names

--- Data Science/Skin Cancer/Prognosis/mrat_rest.py
+++ Data Science/Skin Cancer/Prognosis/mrat_rest.py
@@ -11,31 +11,32 @@ class MelanomaRiskAssessmentTool:
         elif (parameters['gender'] == 'Female'):
             sex = 1
 
-        absolute_risk = 1
+        risk = 1
         if sex == 0:
-            absolute_risk *= MRATConstants.SUNBURN[int(parameters['sunburn'] )]
-            absolute_risk *= MRATConstants.MALE_COMPLEXION[int(parameters['complexion'] )]
-            absolute_risk *= MRATConstants.BIG_MOLES[int(parameters['big-moles'] )]
-            absolute_risk *= MRATConstants.MALE_SMALL_MOLES[int(parameters['small-moles'] )]
-            absolute_risk *= MRATConstants.MALE_FRECKLING[int(parameters['freckling'] )]
-            absolute_risk *= MRATConstants.DAMAGE[int(parameters['damage'] )]
+            risk *= MRATConstants.SUNBURN[int(parameters['sunburn'] )]
+            risk *= MRATConstants.MALE_COMPLEXION[int(parameters['complexion'] )]
+            risk *= MRATConstants.BIG_MOLES[int(parameters['big-moles'] )]
+            risk *= MRATConstants.MALE_SMALL_MOLES[int(parameters['small-moles'] )]
+            risk *= MRATConstants.MALE_FRECKLING[int(parameters['freckling'] )]
+            risk *= MRATConstants.DAMAGE[int(parameters['damage'] )]
         else:
-            absolute_risk *= MRATConstants.TAN[int(parameters['tan'] )]
-            absolute_risk *= MRATConstants.FEMALE_COMPLEXION[int(parameters['complexion'] )]
-            absolute_risk *= MRATConstants.FEMALE_SMALL_MOLES[int(parameters['small-moles'] )]
-            absolute_risk *= MRATConstants.FEMALE_FRECKLING[int(parameters['freckling'] )]
+            risk *= MRATConstants.TAN[int(parameters['tan'] )]
+            risk *= MRATConstants.FEMALE_COMPLEXION[int(parameters['complexion'] )]
+            risk *= MRATConstants.FEMALE_SMALL_MOLES[int(parameters['small-moles'] )]
+            risk *= MRATConstants.FEMALE_FRECKLING[int(parameters['freckling'] )]
 
-        # to get incidence values
+        # to get incidence and mortality values
         age_index = int((age - 20) / 5)
         t1 = age_index * 5 + 20
         t2 = t1 + 5
         incident_rate = MRATConstants.SEX[sex] * MRATConstants.INCIDENCE[sex][age_index]
         mortality_rate = MRATConstants.MORTALITY[sex][age_index]
         
-        risk = incident_rate * absolute_risk * (1 - math.exp((age - t2) * (incident_rate * absolute_risk + mortality_rate))) / (incident_rate * absolute_risk + mortality_rate)
+        absolute_risk = incident_rate * risk * (1 - math.exp((age - t2) * (incident_rate * risk + mortality_rate))) / (incident_rate * risk + mortality_rate)
+
         if age != t1:
-            h12 = MRATConstants.SEX[sex] * MRATConstants.INCIDENCE[sex][age_index + 1]
-            h22 = MRATConstants.MORTALITY[sex][age_index + 1]
-            risk += h12 * absolute_risk * math.exp((age - t2) * (incident_rate * absolute_risk + mortality_rate)) * (1 - math.exp((t1 - age) * (h12 * absolute_risk + h22))) / (h12 * absolute_risk + h22)
-        risk = round(risk * 10000) / 100
+            next_incident_rate = MRATConstants.SEX[sex] * MRATConstants.INCIDENCE[sex][age_index + 1]
+            next_mortality_rate = MRATConstants.MORTALITY[sex][age_index + 1]
+            absolute_risk += next_incident_rate * risk * math.exp((age - t2) * (incident_rate * risk + mortality_rate)) * (1 - math.exp((t1 - age) * (next_incident_rate * risk + next_mortality_rate))) / (next_incident_rate * absolute_risk + next_mortality_rate)
+        absolute_risk = round(risk * 10000) / 100
         ratio = round((risk * 0.01) * 1000)
