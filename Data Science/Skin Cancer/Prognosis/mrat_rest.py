d56a68df91e7b4e0e41d7332f13a66d5ce9c2e06
sabiq <sabiqsabry48@gmail.com>
2021-04-18 15:34:46 +0530

    Create mrat_rest.py
    added male specific risk calculations

---
+++ Data Science/Skin Cancer/Prognosis/mrat_rest.py
@@ -0,0 +1,21 @@
+import math
+import json
+from mrat_constants import MRATConstants
+
+class MelanomaRiskAssessmentTool:
+    @staticmethod
+    def getAbsoluteRisk(parameters, age, sex):
+        sex = None
+        if parameters['gender'] == 'Male':
+            sex = 0
+        elif (parameters['gender'] == 'Female'):
+            sex = 1
+
+        r = 1
+        if sex == 0:
+            r *= MRATConstants.SUNBURN[int(parameters['sunburn'] )]
+            r *= MRATConstants.MALE_COMPLEXION[int(parameters['complexion'] )]
+            r *= MRATConstants.BIG_MOLES[int(parameters['big-moles'] )]
+            r *= MRATConstants.MALE_SMALL_MOLES[int(parameters['small-moles'] )]
+            r *= MRATConstants.MALE_FRECKLING[int(parameters['freckling'] )]
+            r *= MRATConstants.DAMAGE[int(parameters['damage'] )]
