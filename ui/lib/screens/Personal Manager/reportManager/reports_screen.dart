bbb4e030b7403436f07327d24dbd572afe931276
Hammadh Arquil <hammadhy11@gmail.com>
2021-04-18 17:09:17 +0530

    Updated Reports Screen to a Menu

--- ui/lib/screens/Personal Manager/reportManager/reports_screen.dart
+++ ui/lib/screens/Personal Manager/reportManager/reports_screen.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ui/components/custom_app_bar.dart';
 import 'package:ui/components/reports_card.dart';
+import 'package:ui/screens/Personal%20Manager/reportManager/diagnosis_reports_screen.dart';
+import 'package:ui/screens/Personal%20Manager/reportManager/prognosis_reports_screen.dart';
 import 'package:ui/screens/Personal%20Manager/reportManager/report_widgets/ReportListWidget.dart';
 
 import 'api/ReportFirebaseApi.dart';
@@ -24,82 +27,80 @@ class _ReportManagerState extends State<ReportManager> {
           child: Container(
             child:Column(
               children: [
-                Column(
-                  children: [
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 20,
-                            bottom: 8,
-                          ),
-                          child: Align(
-
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              "Reports",
-                              style: TextStyle(
-                                fontFamily: 'Poppins-SemiBold',
-                                fontSize: 32,
-                                color: Color(0xFF00404E)
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 20,
-                              right: 20,
-                              bottom:15
-                          ),
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              "Here you can view your reports",
-                              style: TextStyle(
-                                  fontFamily: 'Poppins-SemiBold',
-                                  fontSize: 13.0,
-                                  color: Color(0xFF3C707B)
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
                 Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Container(
-
-                    child: StreamBuilder<List<Report>>(
-                      stream: ReportFirebaseApi.readReports(),
-                        builder: (context, snapshot) {
-                          switch (snapshot.connectionState) {
-                            case ConnectionState.waiting:
-                              return Center(
-                                  child: CircularProgressIndicator());
-                            default:
-                              print(snapshot);
-                              if (snapshot.hasError) {
-                                return buildText(
-                                    'Something went wrong, Try later');
-                              } else {
-                                final reports = snapshot.data;
-
-                                final provider = Provider.of<ReportProvider>(context);
-                                provider.setReports(reports);
-
-                                return ReportListWidget();
-                              }
-                          }
-                        }
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20
+                  ),
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context)=> DiagnosisReports()
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                          gradient: LinearGradient(
+                            colors: [Color(0xFFC6E7EE), Color(0xFF637477)],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                          ),
+                      ),
+                      padding: EdgeInsets.only(top: 20, left: 25),
+                      width: (MediaQuery.of(context).size.width),
+                      height: 150,
+                      child: Text(
+                        "Diagnosis \nReports",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFF63888F),
+                          fontFamily: 'Poppins-SemiBold',
+                        ),
+                      ),
                     ),
                   ),
                 ),
 
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    vertical: 20
+                  ),
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context)=> PrognosisReports()
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: LinearGradient(
+                          colors: [Color(0xFFC6E7EE), Color(0xFF637477)],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                      ),
+                      padding: EdgeInsets.only(top: 20, left: 25),
+                      width: (MediaQuery.of(context).size.width),
+                      height: 150,
+                      child: Text(
+                        "Prognosis \nReports",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFF63888F),
+                          fontFamily: 'Poppins-SemiBold',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             )
           ),
@@ -108,10 +109,3 @@ class _ReportManagerState extends State<ReportManager> {
     );
   }
 }
-
-Widget buildText(String text) => Center(
-  child: Text(
-    text,
-    style: TextStyle(fontSize: 24, color: Colors.black),
-  ),
-);
