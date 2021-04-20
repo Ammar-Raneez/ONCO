b59ffb95f228685024dcba4cd5d9956267cf0559
Sabiq Sabry <sabiqsabry48@gmail.com>
2021-04-20 17:37:06 +0530

    Update all_meal_screen.dart

--- ui/lib/screens/Meal Plan/all_meal_screen.dart
+++ ui/lib/screens/Meal Plan/all_meal_screen.dart
@@ -5,100 +5,109 @@ import 'package:ui/screens/Meal%20Plan/meal_detail_screen.dart';
 
 class AllMealScreen extends StatelessWidget {
   static String id = "allMealScreen";
-
+  // Container(
+  // child: Text(
+  // "Search"
+  // ),
+  // ),
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: CustomAppBar.arrow(context),
-        body: ListView.builder(
-          shrinkWrap: true,
-            itemCount: RECIPES.length,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MealDetailScreen(
-                        title: RECIPES[index].title,
-                        imgUrl: RECIPES[index].imageUrl,
-                        ingredients: RECIPES[index].ingredients,
-                        steps: RECIPES[index].steps,
-                        duration: RECIPES[index].duration,
-                      ),
-                    ),
-                  );
-                },
-                child: Card(
-                  margin: EdgeInsets.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                    ),
-                  ),
-                  elevation: 4.0,
-                  child: Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: Image.network(
-                          RECIPES[index].imageUrl,
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          height: 150,
+        body: Container(
+          child: ListView.builder(
+            shrinkWrap: true,
+              itemCount: RECIPES.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MealDetailScreen(
+                          title: RECIPES[index].title,
+                          imgUrl: RECIPES[index].imageUrl,
+                          ingredients: RECIPES[index].ingredients,
+                          steps: RECIPES[index].steps,
+                          duration: RECIPES[index].duration,
                         ),
                       ),
-                      Positioned(
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 5,
-                            horizontal: 15,
+                    );
+                  },
+                  child: Card(
+                    margin: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    elevation: 4.0,
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            RECIPES[index].imageUrl,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 180,
                           ),
-                          decoration: BoxDecoration(
-                            color: Colors.black54,
-                          ),
-                          child: Text(
-                            RECIPES[index].title,
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Colors.white,
+                        ),
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 15,
                             ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        right: 0,
-                        child: Container(
-                          decoration: BoxDecoration(
+                            decoration: BoxDecoration(
+                              color: Colors.black87,
                               borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(15),
-                                bottomLeft: Radius.circular(15),
-                              ),
-                              color: Colors.white),
-                          height: 25,
-                          width: 100,
-                          child: Center(
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20)
+                              )
+                            ),
                             child: Text(
-                              '${RECIPES[index].duration}',
+                              RECIPES[index].title,
                               style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Poppins-SemiBold',
+                                fontSize: 19,
+                                color: Colors.white,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        Positioned(
+                          right: 0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Color(0xFF034D5D)),
+                              height: 50,
+                              width: 120,
+                              child: Center(
+                                child: Text(
+                                  '${RECIPES[index].duration}',
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins-SemiBold',
+                                    fontSize: 13,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }),
+                );
+              }),
+        ),
       ),
     );
   }
